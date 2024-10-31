dee3fbc3322fa4b20a19c586890d029b3ca710f0
rollingCheeseWheel <manuelsinner.ms@gmail.com>
2024-10-31 22:22:04 +0100

    Update getMethods.lua

--- getMethods.lua
+++ getMethods.lua
@@ -1,15 +1,43 @@
-local peripheralType = "turbineValve"	-- Change this to the name of the peripheral you want to get the methods from. You can get the peripheral type by running "peripheral.getType(<side>)"
-local outputFolder = "/"	-- Modifying this could lead to errors
-local outputFileName = outputFolder .. "methods_" .. peripheralType	-- The name of the file the methods will be outputted to. Modifying this could lead to errors
-local fileType = "txt"	-- the file type the file will have
-local wrappedPeripheral = peripheral.find(peripheralType)
+local connectedPeripherals = peripheral.getNames()
+if #connectedPeripherals == 0 then
+    print("There are no peripherals connected")
+    return
+end
+local peripheralsTypes = {}
+for i = 1, #connectedPeripherals do
+    peripheralsTypes[connectedPeripherals[i]] = peripheral.getType(connectedPeripherals[i])
+end
+print("Choose peripheral, press enter if peripheral is not in list")
 
-local prettyPeripheral = ""	-- Formatting the k,v table
-for k,v in pairs(wrappedPeripheral) do
-    prettyPeripheral = prettyPeripheral .. "\n" .. k
+local inc = 1   -- format and print all available peripherals as a list to the terminal
+for k,v in pairs(peripheralsTypes) do
+    print(inc .. ":", k, "-", v)
+    inc = inc + 1
 end
 
-local fileHandle, errorMessage = fs.open(outputFileName .. "." .. fileType, "a")	-- "a" flag to append the content to the file, modify at your own risk
+while true do   -- Terminal prompt
+    UserInput = read()
+    if UserInput == "" then
+        print("Connect the peripheral")
+        return
+    end
+    local nUserInput = tonumber(UserInput, 10)
+    if nUserInput ~= nil and 0 < nUserInput and nUserInput <= #connectedPeripherals then
+        UserInput = nUserInput
+        break
+    end
+end
+
+local peripheralType = peripheralsTypes[connectedPeripherals[UserInput]]
+local outputFileName = "/methods_" .. peripheralType .. ".txt"
+local wrappedPeripheral = peripheral.find(peripheralType)
+
+local prettyPeripheral = ""	-- Formatting the wrapped peripheral
+for k,v in pairs(wrappedPeripheral) do
+    prettyPeripheral = k .. "\n" .. prettyPeripheral
+end
+
+local fileHandle, errorMessage = fs.open(outputFileName, "a")	-- "a" flag to append the content to the file, modify at your own risk
 if fileHandle ~= nil then
     fileHandle.write(prettyPeripheral)
     print("Saved methods of peripheral \"" .. peripheralType .. "\" to file \"" .. outputFileName .. "\"")    
