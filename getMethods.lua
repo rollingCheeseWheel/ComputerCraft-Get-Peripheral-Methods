c76c2ff6426bd77b276a6d607a974e760e2c5796
rollingCheeseWheel <manuelsinner.ms@gmail.com>
2024-10-31 22:42:30 +0100

    Update getMethods.lua

--- getMethods.lua
+++ getMethods.lua
@@ -1,24 +1,26 @@
+local pretty = require("cc.pretty")
+ 
 local connectedPeripherals = peripheral.getNames()
 if #connectedPeripherals == 0 then
-    print("There are no peripherals connected")
+    pretty.print(pretty.text("There are no peripherals connected", colors.blue))
     return
 end
 local peripheralsTypes = {}
 for i = 1, #connectedPeripherals do
     peripheralsTypes[connectedPeripherals[i]] = peripheral.getType(connectedPeripherals[i])
 end
-print("Choose peripheral, press enter if peripheral is not in list")
-
+pretty.print(pretty.text("Choose peripheral, press enter if peripheral is not in list", colors.blue))
+ 
 local inc = 1   -- format and print all available peripherals as a list to the terminal
 for k,v in pairs(peripheralsTypes) do
     print(inc .. ":", k, "-", v)
     inc = inc + 1
 end
-
+ 
 while true do   -- Terminal prompt
     UserInput = read()
     if UserInput == "" then
-        print("Connect the peripheral")
+        pretty.print(pretty.text("Exiting", colors.orange))
         return
     end
     local nUserInput = tonumber(UserInput, 10)
@@ -27,21 +29,21 @@ while true do   -- Terminal prompt
         break
     end
 end
-
+ 
 local peripheralType = peripheralsTypes[connectedPeripherals[UserInput]]
 local outputFileName = "/methods_" .. peripheralType .. ".txt"
 local wrappedPeripheral = peripheral.find(peripheralType)
-
-local prettyPeripheral = ""	-- Formatting the wrapped peripheral
+ 
+local prettyPeripheral = "" -- Formatting the wrapped peripheral
 for k,v in pairs(wrappedPeripheral) do
     prettyPeripheral = k .. "\n" .. prettyPeripheral
 end
-
-local fileHandle, errorMessage = fs.open(outputFileName, "a")	-- "a" flag to append the content to the file, modify at your own risk
+ 
+local fileHandle, errorMessage = fs.open(outputFileName, "a")   -- "a" flag to append the content to the file, modify at your own risk
 if fileHandle ~= nil then
     fileHandle.write(prettyPeripheral)
-    print("Saved methods of peripheral \"" .. peripheralType .. "\" to file \"" .. outputFileName .. "\"")    
+    pretty.print(pretty.text("Saved methods of peripheral \"" .. peripheralType .. "\" to file \"" .. outputFileName .. "\"", colors.lime))
+    fileHandle.close()
 else
-    print("Could not save file to \"" .. outputFileName .. "\": " .. errorMessage)
+    pretty.print(pretty.text("Could not save file to \"" .. outputFileName .. "\": " .. errorMessage, colors.orange))
 end
-fileHandle.close()
